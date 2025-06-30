1cb70460195e3c5a18399756e1bf839850782915
YAHW-R <157525442+YAHW-R@users.noreply.github.com>
2025-06-30 15:22:41 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('.header');
+const nav = document.querySelector('nav');
+
+document.addEventListener('DOMContentLoaded', () => {
+    header.classList.add('animate-header');
+    nav.classList.add('animate-nav');
+});
